a0427e717c6caf8bdf79e3ef5a3f7c6739fad252
David Ortiz <da.ortiza1@uniandes.edu.co>
2025-12-03 10:19:49 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarAlbumComponent } from './album/listar-album/listar-album.component';
+import { DetailAlbumComponent } from './album/detail-album/detail-album.component';
+import { ListarBandasComponent } from './bandas/listar-bandas/listar-bandas.component';
+import { DetailBandasComponent } from './bandas/detail-bandas/detail-bandas.component';
+import { ListarCancionesComponent } from './canciones/listar-canciones/listar-canciones.component';
+import { DetailCancionesComponent } from './canciones/detail-canciones/detail-canciones.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'albums', component: ListarAlbumComponent},
+  {path: 'albums/:id', component: DetailAlbumComponent},
+  {path: 'bandas', component: ListarBandasComponent},
+  {path: 'bandas/:id', component: DetailBandasComponent},
+  {path: 'canciones', component: ListarCancionesComponent},
+  {path: 'canciones/:id', component: DetailCancionesComponent},
+  {path: '', redirectTo: '/bandas', pathMatch: 'full'},
+  {path: '**', redirectTo: '/bandas' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
